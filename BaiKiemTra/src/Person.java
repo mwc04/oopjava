5746d28c44bd745c82b66be3b1ff61e1da698537
mwc04 <luongdung1018@gmail.com>
2024-05-22 09:39:51 +0700

    LDD636_Cau2

--- BaiKiemTra/src/Person.java
+++ BaiKiemTra/src/Person.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Person {
     String Name;
     int Age;
     float Luong;
+
+    public void nhapThongTin () {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap ten thong tin");
+        Name = sc.nextLine();
+        System.out.println("nhap Tuoi : ");
+        Age = sc.nextInt();
+        System.out.println("nhap Luong");
+        Luong = sc.nextFloat();
+    }
+    public void inThongTin () {
+        System.out.println("Ten cua ban la : " + Name);
+        System.out.println("Tuoi cua ban la : " + Age);
+        System.out.println("Luong cua ban la : " + Luong);
+    }
 }
